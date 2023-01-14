5653cd8a23c9bfe5e22ed51c6d7f23b734a9e088
Engin Karabudak <Engin.Karabudak@d-teknoloji.com.tr>
2023-01-14 23:23:25 +0300

    tomtom api entegrasyonu ve haritada istanbul

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import * as tt from "@tomtom-international/web-sdk-maps";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const mapElement = useRef();
+  const [map, setMap] = useState({});
+  const mapLongitude = 28.97696;
+  const mapLatitude = 41.00527;
+
+  useEffect(() => {
+    let map = tt.map({
+      key: "IWrAjYGUmAt4iJmyo92edhU1eOb8QxxO",
+      container: mapElement.current,
+      center: [mapLongitude, mapLatitude],
+      zoom: 14,
+    });
+
+    setMap(map);
+  }, []);
+
+  return (
+    <div className="app">
+      <div ref={mapElement} className="map"></div>
+    </div>
+  );
+};
 
 export default App;
