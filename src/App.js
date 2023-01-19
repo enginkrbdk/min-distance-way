77a5397cef9e9cc5491ba2c7eeee35232e8d50fc
Engin Karabudak <Engin.Karabudak@d-teknoloji.com.tr>
2023-01-19 23:18:47 +0300

    some methods upd. and all of them relocated to Map

--- src/App.js
+++ src/App.js
@@ -1,91 +1,17 @@
 import "./App.css";
-import "@tomtom-international/web-sdk-maps/dist/maps.css";
-import { useEffect, useRef, useState } from "react";
 
-import * as tt from "@tomtom-international/web-sdk-maps";
-import * as ttapi from "@tomtom-international/web-sdk-services";
-
+import Map from "./Map";
 const App = () => {
-  const mapElement = useRef();
-  const [map, setMap] = useState({});
-
-  //istanbul koordinarları
-  const [mapLongitude, setLongitude] = useState(28.97696);
-  const [mapLatitude, setLatitude] = useState(41.00527);
-
-  const addTargetMarker = (lngLat, map) => {
-    const element = document.createElement("div");
-    element.className = "marker-delivery";
-    new tt.Marker({
-      element: element,
-    })
-      .setLngLat(lngLat)
-      .addTo(map);
-  };
-
-  useEffect(() => {
-    const origin = {
-      lng: mapLongitude,
-      lat: mapLatitude,
-    };
-
-    let map = tt.map({
-      key: process.env.REACT_APP_TOM_TOM_APP_KEY,
-      container: mapElement.current,
-      center: [mapLongitude, mapLatitude],
-      zoom: 15,
-      stylesVisibility: {
-        trafficFlow: true,
-        trafficIncidents: true,
-      },
-    });
-
-    setMap(map);
-
-    const addMarker = () => {
-      const popupOffset = { bottom: [0, -25] };
-      const element = document.createElement("div");
-      element.className = "marker";
-      const popup = new tt.Popup({ offset: popupOffset }).setHTML(
-        "Şu an buradasın!"
-      );
-      const marker = new tt.Marker({
-        draggable: true,
-        element: element,
-      })
-        .setLngLat([mapLongitude, mapLatitude])
-        .addTo(map);
-
-      marker.on("dragend", () => {
-        const getPosition = marker.getLngLat();
-        setLatitude(getPosition.lat);
-        setLongitude(getPosition.lng);
-      });
-
-      marker.setPopup(popup).togglePopup();
-    };
-
-    addMarker();
-
-    const destinations = [];
-    map.on("click", (e) => {
-      destinations.push(e.lngLat);
-      addTargetMarker(e.lngLat, map);
-    });
-    return () => map.remove();
-  }, [mapLongitude, mapLatitude]);
-
   return (
-    <>
-      {map && (
-        <div className="app">
-          <div ref={mapElement} className="map" />
-          <div className="search-bar">
-            <h1></h1>
+    <div className="wrapper">
+      <main>
+        <div className="wrapper_inner">
+          <div id="lagos">
+            <Map zoom_level={12} travel_mode="truck" />
           </div>
         </div>
-      )}
-    </>
+      </main>
+    </div>
   );
 };
 
--- src/Map.js
+++ src/Map.js
@@ -1,3 +1,192 @@
-export function Map(props) {
-  return <></>;
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
+import * as ttservices from "@tomtom-international/web-sdk-services";
+import tt from "@tomtom-international/web-sdk-maps";
+import { fromSeconds } from "from-seconds";
+import { useEffect, useState, useRef } from "react";
+const data_set_lagos = [
+  { lng: 41.00742, lat: 28.96664, name: "Mimar Hayrettin Caddesi" },
+  { lng: 41.00629, lat: 28.96785, name: "Tülcü Sokak" },
+];
+function Map({ zoom_level, travel_mode }) {
+  const mapElement = useRef();
+  const [map, setMap] = useState(null);
+  const [waypoints] = useState(data_set_lagos);
+
+  //istanbul koordinarları
+  const [mapLongitude, setLongitude] = useState(28.97696);
+  const [mapLatitude, setLatitude] = useState(41.00527);
+  const api_key = process.env.REACT_APP_TOM_TOM_APP_KEY;
+
+  //
+  function create_marker(location) {
+    const marker_el = document.createElement("div");
+    marker_el.id = travel_mode;
+    const popup = new tt.Popup({ offset: 20 }).setText(location.name);
+    const marker = new tt.Marker({ element: marker_el, anchor: "bottom" })
+      .setLngLat([location.lng, location.lat])
+      .setPopup(popup)
+      .addTo(map);
+    return marker;
+  }
+
+  const create_route = (locations) => {
+    ttservices.services
+      .calculateRoute({
+        key: api_key,
+        locations,
+      })
+      .then((routeData) => {
+        const features = routeData.toGeoJson().features;
+        features.forEach((feature, index) => {
+          map.addLayer({
+            id: "route" + index,
+            type: "line",
+            source: {
+              type: "geojson",
+              data: feature,
+            },
+            paint: {
+              "line-color": `red`,
+              "line-opacity": 0.8,
+              "line-width": 6,
+              "line-dasharray": [1, 0, 1, 0],
+            },
+          });
+        });
+      });
+  };
+
+  const optimize_routes = () => {
+    let URL = `https://api.tomtom.com/routing/waypointoptimization/1?key=${api_key}`;
+
+    const data = {
+      waypoints: waypoints.map((location) => {
+        return {
+          point: {
+            latitude: location.lat,
+            longitude: location.lng,
+          },
+        };
+      }),
+      options: {
+        travelMode: travel_mode,
+        vehicleMaxSpeed: 0,
+        vehicleCommercial: true,
+        vehicleLoadType: ["otherHazmatGeneral"],
+        traffic: "live",
+        departAt: "now",
+        outputExtensions: ["travelTimes", "routeLengths"],
+        waypointConstraints: {
+          originIndex: 0,
+          destinationIndex: 0,
+        },
+      },
+    };
+    fetch(URL, {
+      method: "POST",
+      headers: { "Content-type": "application/json;charset=UTF-8" },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const optimized_locations = data.summary.legSummaries.map((summary) => {
+          return { ...waypoints[summary.originIndex], ...summary };
+        });
+        optimized_locations.forEach((location, index) => {
+          const start_time = new Date(location.departureTime).toLocaleString();
+          const arrival_time = new Date(location.arrivalTime).toLocaleString();
+          const distance_in_seconds = fromSeconds(
+            location.travelTimeInSeconds
+          ).toHours();
+          const start_at = location.originIndex;
+          const end_at = location.destinationIndex;
+          const popup = new tt.Popup({ offset: 50 }).setHTML(
+            `<div class="popup">
+            <h1>Location ${index}</h1>
+            <br />
+            <p>Current Point: ${waypoints[start_at].name}</p>
+            <p>Departure Time: ${start_time}</p>
+            <p>Next Stop: ${waypoints[end_at].name}</p>
+            <p>Arrival Time:  ${arrival_time}<p>
+            <p>Distance To next stop:  ${location.lengthInMeters / 1000}(km)</p>
+            <p>Estimated Time To next stop:  ${
+              distance_in_seconds.hours
+            } Hours, ${distance_in_seconds.minutes} Minutes</p>
+            </div> `
+          );
+          create_marker(location).setPopup(popup);
+        });
+        create_route(optimized_locations);
+      });
+  };
+
+  useEffect(() => {
+    let map = tt.map({
+      key: api_key,
+      container: mapElement.current,
+      center: [mapLongitude, mapLatitude],
+      stylesVisibility: {
+        trafficFlow: true,
+        trafficIncidents: true,
+      },
+      zoom: zoom_level,
+    });
+    map.addControl(new tt.FullscreenControl());
+    map.addControl(new tt.NavigationControl());
+    setMap(map);
+
+    const addMarker = () => {
+      const popupOffset = { bottom: [0, -25] };
+      const element = document.createElement("div");
+      element.className = "marker";
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML(
+        "Şu an buradasın!"
+      );
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat([mapLongitude, mapLatitude])
+        .addTo(map);
+
+      marker.on("dragend", () => {
+        const getPosition = marker.getLngLat();
+        setLatitude(getPosition.lat);
+        setLongitude(getPosition.lng);
+
+        //merkezi kaydırdıkça rastgele 5 nokta oluştur
+      });
+
+      marker.setPopup(popup).togglePopup();
+    };
+
+    addMarker();
+    return () => map.remove();
+  }, [mapLongitude, mapLatitude]);
+
+  useEffect(() => {
+    if (map) {
+      map.on("load", () => {
+        waypoints.forEach((location) => {
+          create_marker(location);
+        });
+      });
+    }
+  }, [map]);
+
+  return (
+    <div className="map_wrapper">
+      <button
+        className="btn"
+        disabled={map == null ? true : false}
+        onClick={optimize_routes}
+      >
+        Optimum Rotayı Oluştur
+      </button>
+
+      <div ref={mapElement} className="mapDiv" />
+    </div>
+  );
 }
+
+export default Map;
